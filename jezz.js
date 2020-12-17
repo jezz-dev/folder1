8c6badaa0679095437cb4794e01dc3142052f1a6
seancabalse.dev <seancabalse.dev@gmail.com>
2020-12-17 20:48:21 +0800

    These are the files you worked on

---
+++ jezz.js
@@ -0,0 +1,17 @@
+
+
+alert('this works');
+let userName = 0;
+
+userName = userName + 10;
+outputResult(userName, 'hi there')
+
+
+
+
+
+function outputResult(result,text) {
+    currentResultOutput.textcontent = result;
+    currentCalculationOutput.textcontent = text;
+}
+    
